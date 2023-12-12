ffd7bbfd0565a0a7fb85a22d06d6b10bfe50695b
WuZhiduo <786425304@qq.com>
2023-12-12 22:05:22 +0800

    主界面和场景转换

---
+++ Classes/NormalMap1Scene.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#ifndef _NORMALMAP1SCENE_H__
+#define __NORMALMAP1SCENE_H__
+
+#include "cocos2d.h"
+
+
+class NormalMap1 : public cocos2d::Layer
+{
+public:
+   //创建场景
+    static cocos2d::Scene* createScene();
+
+    //创建瓦片地图
+    static cocos2d::TMXTiledMap* createMap();
+
+    virtual bool init();
+
+    
+
+    // implement the "static create()" method manually
+    CREATE_FUNC(NormalMap1);
+};
+
+#endif // _NORMALMAP1SCENE_H__
