0614606569a447c3c0ff1fee0f75cf40f0317216
ryl1ie <1239716933@qq.com>
2023-12-21 22:08:01 +0800

    12.21初始化

---
+++ Classes/GameManager.h
@@ -0,0 +1,41 @@
+#ifndef _GAME_MANAGER_H_
+#define _GAME_MANAGER_H_
+
+#include <iostream>
+#include "cocos2d.h"
+#include "EnemyBase.h"
+#include "GanYuanBase.h"
+#include "Const.h"
+//#include "Wave.h"
+
+
+USING_NS_CC;
+
+class GameManager
+{
+public:
+	Scene* gameScene;
+	Vector<EnemyBase*> enemyVector;
+	Vector<Sprite*> bulletVector;
+	Vector<GanYuanBase*> ganyuanVector;
+	//Vector<Wave*> waveVector;
+	CC_SYNTHESIZE(int, money, Money);
+	CC_SYNTHESIZE(int, groupNum, GroupNum);
+	CC_SYNTHESIZE(std::string, curMapName, CurMapName);
+	CC_SYNTHESIZE(std::string, currLevelFile, CurrLevelFile);
+	CC_SYNTHESIZE(std::string, nextLevelFile, NextLevelFile);
+	CC_SYNTHESIZE(bool, isFinishedAddGroup, IsFinishedAddGroup);
+	CC_SYNTHESIZE(std::string, curBgName, CurBgName);
+
+	bool init();
+	void clear();
+
+	static GameManager* getInstance();
+
+private:
+
+	static GameManager* instance;
+
+};
+#endif /* defined(_GAME_MANAGER_H_) */
+
---
+++ Classes/GanYuanShield.h
@@ -0,0 +1,51 @@
+#ifndef _GAN_YUAN_SHIELD_H_
+#define _GAN_YUAN_SHIELD_H_
+/*
+   CC_SYNTHESIZE(int, scope, Scope);  // 塔的视线范围
+   CC_SYNTHESIZE(int, lethality, Lethality);   // 杀伤力
+   CC_SYNTHESIZE(float, hp, Hp);  // 最大血量
+   CC_SYNTHESIZE(float, health, Health);  // 当前血量*/
+#include "GanYuanBase.h"
+#include "Const.h"
+class GanYuanBase;
+
+class GanYuanShield : public GanYuanBase
+{
+public:
+
+    CC_SYNTHESIZE(int, block, Block);  //阻挡数
+    CC_SYNTHESIZE(int, curBlock, CurBlock);  //已经阻挡数
+    //base中已有
+    
+    //完成初始化
+    //开始攻击
+    CC_SYNTHESIZE(Animation*, attackAnimation, AttackAnimation);//攻击时的动画
+    CC_SYNTHESIZE(Animation*, bigMoveAnimation, BigMoveAnimation);//大招时的动画
+
+    
+    
+    bool init();
+    static GanYuanShield* create();//创建静态create方法
+    virtual void setDefaultData();//设置默认数值
+    virtual void positionLegal(bool& state);;//当前位置是否合法
+    virtual void initial();//初始化
+    
+    //完成初始化，包括内置数值初始化以及动画初始化
+    bool attack();//攻击
+    bool bigMove();//是否达到大招
+    bool castBigMove();//开放大招
+    //冷却状态
+    bool beingCool();//冷却状态动画显示及自己退出冷却状态
+    //reset
+    bool reset();//冷却后重启
+
+private:
+    void updateHealth(float delta);//更新血条
+    void updateLethality(float delta);//更新杀伤力（大招释放）
+    void updatDefence(float delta);//更新防御力（技能加成）
+
+    Sprite* ganyuanShield;
+
+};
+
+#endif /* defined(_GAN_YUAN_SHIELD_H_) */
